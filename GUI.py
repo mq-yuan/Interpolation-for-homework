7a91d84920e94b538c2f978a49fedeb242e4fafb
iLGZH <dearmengqiyuan@163.com>
2023-03-24 14:12:44 +0800

    first commit

---
+++ GUI.py
@@ -0,0 +1,162 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import NearestNDInterpolator
+from scipy.interpolate import RBFInterpolator
+from skimage.metrics import mean_squared_error
+from skimage.metrics import peak_signal_noise_ratio
+from skimage.metrics import structural_similarity
+
+
+def select_file(select_path):
+    # 单个文件选择
+    selected_file_path = filedialog.askopenfilename()  # 使用askopenfilename函数选择单个文件
+    select_path.set(selected_file_path)
+
+
+def Interpolator(img_path, mask_path, random_ratio, flag):
+    img = cv2.imread(img_path)
+
+    if flag == 1:
+        mask_path = f'./Randomly remove {random_ratio}%.bmp'
+        mask = np.ones(img.shape[:2]) * 255
+        all_point = np.transpose(np.meshgrid(np.arange(mask.shape[0]), np.arange(mask.shape[1]))).reshape((-1, 2))
+        res = np.random.choice(all_point.shape[0], size=(int(all_point.shape[0] * (random_ratio/100))), replace=False)
+        mask[tuple(np.transpose(all_point[res]))] = 0
+    else:
+        mask = cv2.cvtColor(cv2.imread(mask_path), cv2.COLOR_BGR2GRAY)
+
+    location = np.transpose(np.nonzero(mask))  # mask白色区域
+    x = np.transpose(np.where(mask == 0))  # mask黑色区域
+    data = img[np.nonzero(mask)]  # mask白色区域对应的img像素值
+
+    # 插值
+    y_RBF_GAUSSIAN = RBFInterpolator(location, data, neighbors=100, kernel='gaussian', epsilon=1)(x)  # RBF Gaussian
+    y_RBF_TPS = RBFInterpolator(location, data, neighbors=100, kernel='thin_plate_spline')(x)  # RBF TPS
+    y_NNI = NearestNDInterpolator(location, data)(x)  # Nearest neighbor interpolation
+    y_BNI = LinearNDInterpolator(location, data)(x)  # Bilinear interpolation
+
+    # 拼接
+    masked = np.zeros(img.shape)
+    masked[np.nonzero(mask)] = data
+
+    ans_RBF_GAUSSIAN = np.zeros(img.shape)
+    ans_RBF_GAUSSIAN[np.nonzero(mask)] = data
+    ans_RBF_GAUSSIAN[np.where(mask == 0)] = y_RBF_GAUSSIAN
+
+    ans_RBF_TPS = np.zeros(img.shape)
+    ans_RBF_TPS[np.nonzero(mask)] = data
+    ans_RBF_TPS[np.where(mask == 0)] = y_RBF_TPS
+
+    ans_NNI = np.zeros(img.shape)
+    ans_NNI[np.nonzero(mask)] = data
+    ans_NNI[np.where(mask == 0)] = y_NNI
+
+    ans_BNI = np.zeros(img.shape)
+    ans_BNI[np.nonzero(mask)] = data
+    ans_BNI[np.where(mask == 0)] = y_BNI
+
+    # 保存
+    img_name = os.path.splitext(os.path.split(img_path)[1])[0]
+    mask_name = os.path.splitext(os.path.split(mask_path)[1])[0]
+    img_dir = os.path.join('./Ans/', img_name)
+    mask_dir = os.path.join(img_dir, mask_name)
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+    if not os.path.exists(mask_dir):
+        os.makedirs(mask_dir)
+    cv2.imwrite(os.path.join(mask_dir, 'Orignal.bmp'), img)
+    cv2.imwrite(os.path.join(mask_dir, 'Masked.bmp'), masked)
+    cv2.imwrite(os.path.join(mask_dir, 'RBF_GAUSSIAN.bmp'), ans_RBF_GAUSSIAN)
+    cv2.imwrite(os.path.join(mask_dir, 'RBF_TPS.bmp'), ans_RBF_TPS)
+    cv2.imwrite(os.path.join(mask_dir, 'NNI.bmp'), ans_NNI)
+    cv2.imwrite(os.path.join(mask_dir, 'BNI.bmp'), ans_BNI)
+
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8),
+                             sharex=True, sharey=True)
+    ax = axes.ravel()
+
+    ax[0].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., img / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., img / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., img / 255., data_range=1, channel_axis=2):.3f}')
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    ax[0].set_title('Original image')
+    img = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'Orignal.bmp')), cv2.COLOR_BGR2RGB)
+    ax[0].imshow(img)
+
+    ax[1].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., ans_NNI / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., ans_NNI / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., ans_NNI / 255., data_range=1, channel_axis=2):.3f}')
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
+    ax[1].set_title('NearestNDInterpolator')
+    ans_NNI = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'NNI.bmp')), cv2.COLOR_BGR2RGB)
+    ax[1].imshow(ans_NNI)
+
+    ax[2].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., ans_BNI / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., ans_BNI / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., ans_BNI / 255., data_range=1, channel_axis=2):.3f}')
+    ax[2].set_xticks([])
+    ax[2].set_yticks([])
+    ax[2].set_title('Bilinear interpolation')
+    ans_BNI = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'BNI.bmp')), cv2.COLOR_BGR2RGB)
+    ax[2].imshow(ans_BNI)
+
+    ax[3].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., masked / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., masked / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., masked / 255., data_range=1, channel_axis=2):.3f}')
+    ax[3].set_xticks([])
+    ax[3].set_yticks([])
+    ax[3].set_title('Masked image')
+    masked = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'Masked.bmp')), cv2.COLOR_BGR2RGB)
+    ax[3].imshow(masked)
+
+    ax[4].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., ans_RBF_TPS / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., ans_RBF_TPS / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., ans_RBF_TPS / 255., data_range=1, channel_axis=2):.3f}')
+    ax[4].set_xticks([])
+    ax[4].set_yticks([])
+    ax[4].set_title('RBF thin_plate_spline')
+    ans_RBF_TPS = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'RBF_TPS.bmp')), cv2.COLOR_BGR2RGB)
+    ax[4].imshow(ans_RBF_TPS)
+
+    ax[5].set_xlabel(
+        f'MSE: {mean_squared_error(img / 1., ans_RBF_GAUSSIAN / 1.):.2f}, PSNR: {peak_signal_noise_ratio(img / 255., ans_RBF_GAUSSIAN / 255., data_range=1):.2f}, SSIM: {structural_similarity(img / 255., ans_RBF_GAUSSIAN / 255., data_range=1, channel_axis=2):.3f}')
+    ax[5].set_xticks([])
+    ax[5].set_yticks([])
+    ax[5].set_title('RBF Gaussian')
+    ans_RBF_GAUSSIAN = cv2.cvtColor(cv2.imread(os.path.join(mask_dir, 'RBF_GAUSSIAN.bmp')), cv2.COLOR_BGR2RGB)
+    ax[5].imshow(ans_RBF_GAUSSIAN)
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(mask_dir, 'Ans.png'), dpi=400)
+    plt.show()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    img_path = tk.StringVar()
+    mask_path = tk.StringVar()
+    random_remove = tk.IntVar()
+    var = tk.IntVar()
+    random_remove.set(10)
+
+    tk.Label(root, text="img path：").pack(side='top', anchor='nw')
+    tk.Entry(root, textvariable=img_path).pack(side='top', anchor='n', fill='x')
+    tk.Button(root, text="select img", command=lambda: select_file(img_path)).pack(side='top', anchor='ne')
+
+    tk.Label(root, text="mask path：").pack(side='top', anchor='nw')
+    tk.Entry(root, textvariable=mask_path).pack(side='top', anchor='n', fill='x')
+    tk.Button(root, text="select mask", command=lambda: select_file(mask_path)).pack(side='top', anchor='ne')
+
+    tk.Label(root, text="randomly remove ratio(int %): ").pack(side='top', anchor='nw')
+    tk.Entry(root, textvariable=random_remove).pack(side='top', anchor='n', fill='x')
+
+    tk.Label(root, text="Select the mode in which the image is destroyed: ").pack(side='top', anchor='nw')
+    tk.Radiobutton(root, text="Mask image", variable=var, value=0, command=None).pack(side='top', anchor='nw')
+    tk.Radiobutton(root, text="Randomly remove ", variable=var, value=1, command=None).pack(side='top', anchor='nw')
+
+    tk.Button(root, text="Interpolator", command=lambda: Interpolator(img_path.get(), mask_path.get(), random_remove.get(), var.get())).pack()
+
+    root.mainloop()
---
+++ README.md
@@ -0,0 +1,77 @@
+![](https://typora-ilgzh.oss-cn-beijing.aliyuncs.com/202303241405754.png)
+
+# 图像插值
+
+## 图像插值理论
+
+插值指的是利用已知数据去预测未知数据，图像插值则是给定一个像素点，根据它周围像素点的信息来对该像素点的值进行预测，如何给这些值赋值，就是图像插值所要解决的问题。
+
+本次实验主要关注：最近邻插值(Nearest neighbor interpolation)、双线性插值(Bilinear interpolation)和径向基函数插值(RBF interpolation)，其中径向基函数插值选用TPS(thin plate spline)和Gaussian两种径向基函数。
+
+## 环境依赖
+
+- `pip install -r requirements.txt`.
+
+## 测试
+
+在windows系统且确保已安装python3的情况下，点击`run_me.bat`文件可直接运行。
+
+## 文件架构
+
+测试用图片中已附带`Image`文件夹如下：
+
+```txt
+Image
+├─person # 个人图片
+|   ├─person_01.bmp
+|   ├─person_02.bmp
+|   └person_03.bmp
+├─mask # 遮挡用图片
+|  ├─butterfly_words_1.bmp # 文字遮挡
+|  ├─butterfly_words_2.bmp # 文字遮挡
+|  ├─Random scribbles_1.bmp # 随机涂鸦
+|  └Random scribbles_2.bmp # 随机涂鸦
+├─example # 图像处理常用图片
+|    ├─example_01.tiff
+|    ├─example_02.bmp
+|    └example_03.bmp
+```
+
+一共有两种测试方法：1. 根据mask遮挡进行插值 2. 随机丢弃一定比例的像素点后进行插值。
+
+- 根据mask遮挡插值：先在`Image/person`或`Image/example`中选择测试图片，后在`Image/mask`中选择遮挡图片，最后选择mask模式。
+- 随即丢弃插值：在`Image/person`或`Image/example`中选择测试图片，输入随机丢弃像素点比例，最后选择随机丢弃模式。
+
+结果会保存在`Ans`文件夹下，文件夹结构如下：
+
+```txt
+Ans
+├─person_02 # 人测试图片文件夹2
+|     ├─Randomly remove 20% # 随即丢弃20%文件夹
+|     |          ├─Ans.png # 结果比较图片
+|     |          ├─BNI.bmp # BNI插值图片
+|     |          ├─Masked.bmp # 遮挡或丢弃后图片
+|     |          ├─NNI.bmp # NNI插值图片
+|     |          ├─Orignal.bmp #  原始图片
+|     |          ├─RBF_GAUSSIAN.bmp # RBF_GAUSSIAN插值图片
+|     |          └RBF_TPS.bmp # RBF_TPS插值图片
+|     ├─Randomly remove 10% # 随机丢弃10%文件夹
+|     |          ├─....... # 同上
+|     ├─Random scribbles_2 # 随机涂鸦2文件夹
+|     |          ├─....... # 同上
+|     ├─Random scribbles_1 # 随机涂鸦1文件夹
+|     |          ├─....... # 同上
+|     ├─butterfly_words_2 # 文字遮挡2文件夹
+|     |          ├─....... # 同上
+|     ├─butterfly_words_1 # 文字遮挡1文件夹
+|     |          └....... # 同上
+├─person_01 # 个人测试图片文件夹1
+|     ├─...... # 同上
+├─example_03
+|     ├─......
+├─example_02
+|     ├─......
+```
+
+**注意**：测试图片尽量在512*512以下，否则计算时间会过长。
+
